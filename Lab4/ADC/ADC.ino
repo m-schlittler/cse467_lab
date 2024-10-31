4ebbeb10dc48b76f4abbf8626344f48c9336e4c2
Carmen Bland Jr <112424349+washu-blandjr@users.noreply.github.com>
2024-10-30 19:28:37 -0500

    Update ADC.ino

--- Lab4/ADC/ADC.ino
+++ Lab4/ADC/ADC.ino
@@ -9,21 +9,28 @@ int val_squ = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  // part one -- read values
+  // read values
   val_sin = analogRead(SIN);
   val_squ = analogRead(SQU);
 
-  // part three -- try with different baud rates
-  // Setting a new baud rate
-  Serial.flush();
-  Serial.end();
-  Serial.begin(115200);
-  sprintf("Sine Wave value: %0.5f V; Square Wave value: %0.5f", val_sin, val_squ);
-  delay(10); // Small delay for readability -- thanks GPT!
+  char buffer[100];
+  // int count = sprintf(buffer, "Sine Wave value: %f V; Square Wave value: %f\r\n", sin_f, squ_f);
+  // Serial.write(buffer, count);
+  
+  float sin_f = (val_sin/1024.0)*5.0;
+  float squ_f = (val_squ/1024.0)*5.0;
+  
+  Serial.print("Sine_Wave_value:");
+  Serial.print(sin_f, 2);
+  Serial.print(",");
+  Serial.print("Square_Wave_value:");
+  Serial.print(squ_f, 2);
+  Serial.println("");
+  delay(10); // Small delay for readability
 } 
