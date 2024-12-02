2e2610fcd7d29d01454b18774d8a0d5e0c86c0b0
Carmen Bland Jr <112424349+washu-blandjr@users.noreply.github.com>
2024-12-01 19:49:11 -0600

    Added lcd logic to temperatureControl.ino

--- FinalProject/temperatureControl/temperatureControl.ino
+++ FinalProject/temperatureControl/temperatureControl.ino
@@ -1,8 +1,15 @@
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
+// pin setup
+LiquidCrystal_I2C lcd(39, 20, 4);
+const uint8_t BTN_COLD = 2;   // Digital 2
+const uint8_t BTN_HOT = 3;    // Digital 3
+const uint8_t led = 13;       // led test
+
 float currentTemp;
-float setTemp = 20;
+float setTemp = 20; // temp as set in ISR
+bool ISRchanged = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -18,19 +25,32 @@ void setup() {
   Serial.println("Init LED");
   initLED();
 
-  Serial.println("Init Timer");
-  TCCR1A = 0;
-  TCCR1B = 0;
-  TCCR1B |= 0x04;
-  TCNT1 = 3035;
-  TIMSK1 |= 0x01;
+  // Serial.println("Init Timer");
+  // TCCR1A = 0;
+  // TCCR1B = 0;
+  // TCCR1B |= 0x04;
+  // TCNT1 = 3035;
+  // TIMSK1 |= 0x01;
 
   Serial.println("Init Done");
+
+  // lcd setup
+  lcd.init();
+  lcd.backlight();
+  
+  // pin init
+  pinMode(BTN_COLD, INPUT);
+  pinMode(BTN_HOT, INPUT);
+  pinMode(led, OUTPUT);
+
+  // button interrupt
+  // attachInterrupt(digitalPinToInterrupt(BTN_COLD), C_BTN, RISING); // allow for interrupt on button press
+  attachInterrupt(digitalPinToInterrupt(BTN_HOT), setPoint_interrupt, RISING); // allow for interrupt on button press
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  #if false
+  #if true
   setColor('R');
   Serial.println("R");
   delay(1000);
@@ -41,9 +61,24 @@ void loop() {
   Serial.println("B");
   delay(1000);
 
-  float temp = readTemperature();
+  currentTemp = readTemperature();
+
+  lcd.setCursor(0,0); // set pos for LCD
+  lcd.print("Temp:");
+  lcd.setCursor(6,0); // set pos for LCD
+  lcd.print(currentTemp);
+
   Serial.print("Temp: ");
-  Serial.println(temp);
+  Serial.println(currentTemp);
+
+  if (ISRchanged) {  
+    lcd.setCursor(0, 1);
+    lcd.print("Setpoint:");
+    lcd.setCursor(10, 1);
+    lcd.print(setTemp);
+    ISRchanged = false;
+  }
+
   #endif
   delay(750);
 }
@@ -81,5 +116,10 @@ ISR(TIMER1_OVF_vect)
   handleInterrupt();
 }
 
+// places setpoint on lcd screen
+void setPoint_interrupt(){
+  setTemp = currentTemp;
+  ISRchanged = true;
+}
 
 
--- libraries/LiquidCrystal/examples/HelloWorld/HelloWorld.ino
+++ libraries/LiquidCrystal/examples/HelloWorld/HelloWorld.ino
@@ -1,5 +1,9 @@
-/*
-  LiquidCrystal Library - Hello World
+/*LCD-Hello_world.ino
+
+ Modified By: Ho Yun "Bobby" Chan
+ SparkFun Electronics
+ Date: 5/22/2019
+ License: This code is public domain.
 
  Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
  library works with all LCD displays that are compatible with the
@@ -7,21 +11,25 @@
  can usually tell them by the 16-pin interface.
 
  This sketch prints "Hello World!" to the LCD
- and shows the time.
+ and shows the time since the Arduino was turned on.
 
-  The circuit:
- * LCD RS pin to digital pin 12
- * LCD Enable pin to digital pin 11
- * LCD D4 pin to digital pin 5
- * LCD D5 pin to digital pin 4
- * LCD D6 pin to digital pin 3
- * LCD D7 pin to digital pin 2
- * LCD R/W pin to ground
- * LCD VSS pin to ground
- * LCD VCC pin to 5V
- * 10K resistor:
- * ends to +5V and ground
- * wiper to LCD VO pin (pin 3)
+  Hardware Hookup:
+
+    lCD VSS pin to GND
+    LCD VCC pin to 5V
+    10kΩ Potentiometer to LCD VO pin (pin 3)
+    LCD RS pin to digital pin 13
+    LCD R/W pin to GND
+    LCD Enable pin to digital pin 12
+    .
+    .
+    .
+    LCD D4 pin to digital pin 11
+    LCD D5 pin to digital pin 10
+    LCD D6 pin to digital pin 9
+    LCD D7 pin to digital pin 8
+    LCD-Backlight - Anode to 10KΩ resistor to +5V (optional depending on your LCD)
+    LCD Backlight - K to GND
 
  Library originally added 18 Apr 2008
  by David A. Mellis
@@ -34,32 +42,32 @@
  modified 7 Nov 2016
  by Arturo Guadalupi
 
- This example code is in the public domain.
+ http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld=
 
- http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
+https://learn.sparkfun.com/tutorials/basic-character-lcd-hookup-guide
 
 */
 
 // include the library code:
 #include <LiquidCrystal.h>
 
-// initialize the library by associating any needed LCD interface pin
-// with the arduino pin number it is connected to
-const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+//initialize the library by associating any needed LCD interface pin
+//with the arduino pin number it is connected to
+const int rs = 13, en = 12, d4 = 11, d5 = 10, d6 = 9, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  // set up the LCD's number of columns and rows:
+  //set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+  // lcd.setCursor(0, 0);
+  //Print a message to the LCD.
+  lcd.print("Hi");
 }
 
 void loop() {
-  // set the cursor to column 0, line 1
+  //set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
   // print the number of seconds since reset:
   lcd.print(millis() / 1000);
 }
-
